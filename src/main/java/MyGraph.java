c1679659faf241bcb0942bac9340286f539524a7
qli627 <qli627@aucklanduni.ac.nz>
2022-09-23 22:40:36 +1200

    write the critical path and bottom level in MyGraph

--- src/main/java/MyGraph.java
+++ src/main/java/MyGraph.java
@@ -9,59 +9,79 @@ public class MyGraph {
 
     Graph graph;
 
-     public MyGraph(Graph graph){
-         this.graph = graph;
-     }
+    public MyGraph(Graph graph) {
+        this.graph = graph;
+    }
 
-     public Node getNodeByValue(String node){
-         return graph.getNode(node);
-     }
+    public Node getNodeByValue(String node) {
+        return graph.getNode(node);
+    }
 
-     //get Node weight
-     public Double getNodeWeight(String node){
-         return Double.parseDouble(graph.getNode(node).getAttribute("Weight").toString());
-     }
+    //get Node weight
+    public Double getNodeWeight(String node) {
+        return Double.parseDouble(graph.getNode(node).getAttribute("Weight").toString());
+    }
 
-     //get the edge sourceNode -> destNode
-     public Edge getEdge(String sourceNode, String destNode){
-          return graph.getEdge("("+sourceNode+";"+destNode+")");
-     }
+    //get the edge sourceNode -> destNode
+    public Edge getEdge(String sourceNode, String destNode) {
+        return graph.getEdge("(" + sourceNode + ";" + destNode + ")");
+    }
 
-     //get Edge Weight
-    public Double getEdgeWeight(String sourceNode,String destNode){
-         return Double.parseDouble(getEdge(sourceNode,destNode).getAttribute("Weight").toString());
+    //get Edge Weight
+    public Double getEdgeWeight(String sourceNode, String destNode) {
+        return Double.parseDouble(getEdge(sourceNode, destNode).getAttribute("Weight").toString());
     }
 
     //get list of the node which in-degree is 0
-    public List<Node> getAllStartNode(){
-         List<Node> res = new ArrayList<>();
-         for(Node d:graph){
-             if(d.getInDegree()==0){
-                 res.add(d);
-             }
-         }
-         return res;
+    public List<Node> getAllStartNode() {
+        List<Node> res = new ArrayList<>();
+        for (Node d : graph) {
+            if (d.getInDegree() == 0) {
+                res.add(d);
+            }
+        }
+        return res;
     }
 
     //get all children node of this input node
-    public List<Node> getAllChildrenNode(Node d){
-         List<Node> res = new ArrayList<>();
-         for(Edge e:d){
-             if(e.getNode0() == d){
-                 res.add(e.getNode1());
-             }
-         }
-         return res;
+    public List<Node> getAllChildrenNode(Node d) {
+        List<Node> res = new ArrayList<>();
+        for (Edge e : d) {
+            if (e.getNode0() == d) {
+                res.add(e.getNode1());
+            }
+        }
+        return res;
     }
 
     //get all Parent Node of this input node
-    public List<Node> getAllParentNode(Node d){
-         List<Node> res = new ArrayList<>();
-         for(Edge e:d){
-             if(e.getNode1() == d){
-                 res.add(e.getNode0());
-             }
-         }
-         return res;
+    public List<Node> getAllParentNode(Node d) {
+        List<Node> res = new ArrayList<>();
+        for (Edge e : d) {
+            if (e.getNode1() == d) {
+                res.add(e.getNode0());
+            }
+        }
+        return res;
+    }
+
+    //get the bottom level of the node
+    public double getBottomLevel(Node d) {
+        double ans = getNodeWeight(d.getId());
+
+        for(Node children:getAllChildrenNode(d)){
+             ans = Math.max(ans,getNodeWeight(d.getId())+getBottomLevel(children));
+        }
+        return ans;
+    }
+
+    //get the critical path of the graph
+    public double getCriticalPath() {
+        double criticalPath = 0;
+        for(Node d:getAllStartNode()){
+            criticalPath = Math.max(criticalPath, getBottomLevel(d));
+        }
+
+        return criticalPath;
     }
 }
