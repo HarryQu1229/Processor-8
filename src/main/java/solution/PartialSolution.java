94df332e877dfe4bcf3aaa906d7c0b7bdc9d7dda
tgsmikema <sma148@aucklanduni.ac.nz>
2022-09-25 10:43:12 +0800

    fix / logical bug for calculating starting time

--- src/main/java/solution/PartialSolution.java
+++ src/main/java/solution/PartialSolution.java
@@ -18,13 +18,12 @@ public class PartialSolution {
     private List<Node> nodesPath;
 
 
-
-    public PartialSolution(PartialSolution prevPartial, Digraph graph, Node currentNode, int processorNo){
+    public PartialSolution(PartialSolution prevPartial, Digraph graph, Node currentNode, int processorNo) {
 
         this.nodesPath = new ArrayList<>();
         nodesPath.addAll(prevPartial.getNodesPath());
         this.nodeStates = new LinkedHashMap<>();
-        for (Node node : prevPartial.getNodeStates().keySet()){
+        for (Node node : prevPartial.getNodeStates().keySet()) {
             List<Integer> states = new ArrayList<>();
             states.add(prevPartial.getNodeStates().get(node).get(PROCESSOR_NO)); //processor number
             states.add(prevPartial.getNodeStates().get(node).get(STARTING_TIME)); //starting time
@@ -32,22 +31,35 @@ public class PartialSolution {
             nodeStates.put(node, states);
         }
 
-        if (!nodesPath.isEmpty()){
+        if (!nodesPath.isEmpty()) {
             // get the previous node
-            Node prevNode = nodesPath.get(nodesPath.size()-1);
+            Node prevNode = nodesPath.get(nodesPath.size() - 1);
 
             // previous processor number
             int prevProcessorNo = nodeStates.get(prevNode).get(PROCESSOR_NO);
             int prevStartingTime = nodeStates.get(prevNode).get(STARTING_TIME);
             int prevNodeWeight = graph.getNodeWeight(prevNode.getId()).intValue();
 
-            if(prevProcessorNo == processorNo){
-                nodeStates.get(currentNode).set(STARTING_TIME, prevStartingTime + prevNodeWeight);
-            } else {
-                //System.out.println(prevNode.getId() + " " + currentNode.getId());
-                int communicationCost = graph.getEdgeWeight(prevNode.getId(), currentNode.getId()).intValue();
-                nodeStates.get(currentNode).set(STARTING_TIME, prevStartingTime + prevNodeWeight + communicationCost);
+
+            int startTime = 0;
+            for (Node node : nodesPath) {
+                if (node.hasEdgeToward(currentNode)) {
+                    if (nodeStates.get(node).get(PROCESSOR_NO) == processorNo) {
+                        startTime = (int) Math.max(startTime, nodeStates.get(node).get(STARTING_TIME) + graph.getNodeWeight(node.getId()));
+                    } else {
+                        int communicationCost = graph.getEdgeWeight(node.getId(), currentNode.getId()).intValue();
+                        startTime = (int) Math.max(startTime, nodeStates.get(node).get(STARTING_TIME) + graph.getNodeWeight(node.getId()) + communicationCost);
+                    }
+                }
             }
+
+            int finishTimePrev = 0;
+            for (Node node: nodesPath){
+                if (nodeStates.get(node).get(PROCESSOR_NO) == processorNo){
+                    finishTimePrev = (int)Math.max(finishTimePrev, nodeStates.get(node).get(STARTING_TIME)+graph.getNodeWeight(node.getId()));
+                }
+            }
+            nodeStates.get(currentNode).set(STARTING_TIME, Math.max(startTime, finishTimePrev));
         }
 
         // add this node to the solution path
@@ -59,38 +71,38 @@ public class PartialSolution {
         // decrease all direct children indegree by 1 (effectively get rid of the in edges from the current node to
         // all its children nodes).
         List<Node> childrenNodes = graph.getAllChildrenNode(currentNode);
-        for(Node childNode : childrenNodes){
+        for (Node childNode : childrenNodes) {
             int currentIndegree = nodeStates.get(childNode).get(INDEGREE_NO);
-            nodeStates.get(childNode).set(INDEGREE_NO, currentIndegree-1);
+            nodeStates.get(childNode).set(INDEGREE_NO, currentIndegree - 1);
         }
 
     }
 
     // initial empty state, set up all nodes.
-    public PartialSolution(Digraph graph){
+    public PartialSolution(Digraph graph) {
         this.nodesPath = new ArrayList<>();
         this.nodeStates = new LinkedHashMap<>();
 
-        for (Node node: graph.getAllNodes()){
+        for (Node node : graph.getAllNodes()) {
             List<Integer> states = new ArrayList<>();
             states.add(0); //processor number
             states.add(0); //starting time
-            states.add(graph.getAllParentNode(node).size()); // indegree
+            states.add(node.getInDegree()); // indegree
             nodeStates.put(node, states);
         }
     }
 
-    public List<Node> getAvailableNextNodes(){
+    public List<Node> getAvailableNextNodes() {
         List<Node> availableNextNodes = new ArrayList<>();
-        for (Node node: nodeStates.keySet()){
-            if (nodeStates.get(node).get(INDEGREE_NO) == 0){
+        for (Node node : nodeStates.keySet()) {
+            if (nodeStates.get(node).get(INDEGREE_NO) == 0) {
                 availableNextNodes.add(node);
             }
         }
         return availableNextNodes;
     }
 
-    public List<Node> getNodesPath(){
+    public List<Node> getNodesPath() {
         return nodesPath;
     }
 
