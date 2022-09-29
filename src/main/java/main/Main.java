9cc3577b673bc215e6bd25a86ac07000ac6530b5
tgsmikema <sma148@aucklanduni.ac.nz>
2022-09-29 22:53:25 +0800

    Refactor / refactor out unused testing code in `Main`

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,15 +4,14 @@ import io.InputLoader;
 import org.graphstream.graph.Node;
 import solution.AStar;
 import solution.Digraph;
+import solution.SolutionTree;
 
 public class Main {
 
     public static void main(String[] args) {
 //      InputLoader.print(graph, true);
-        InputLoader.loadDotFile("g10");
+        InputLoader.loadDotFile("g1");
 
-
-//        digraph.init();
 
 
 //        InputLoader.print(digraph, false);
@@ -23,38 +22,23 @@ public class Main {
 //        System.out.println(solutionTree.getAllNodes().size());
 //        InputLoader.print(solutionTree, false);
 
-//        System.out.println(digraph.getAllNodes());
 
-//        System.out.println(digraph.getBottomLevel(digraph.getNodeByValue("h")));
 
-//        System.out.println(myGraph.getCriticalPath());
 
-//        int numOfProcessors = 2;
 //
-//        SolutionTree solutionTree = new SolutionTree(digraph,2);
+//        SolutionTree solutionTree = new SolutionTree(2);
 //        solutionTree.build(solutionTree.getRoot());
 //        solutionTree.print();
 
-//        System.out.println(digraph.getCriticalPath());
 
 
-//
         AStar aStar = new AStar();
         String s = aStar.buildTree(2);
         System.out.println(s);
 
-//        System.out.println(digraph.getCriticalPath());
-//        System.out.println(digraph.getBottomLevel(digraph.getNode("1")));
-//        PriorityQueue<PartialSolution> test = aStar.test();
-//        while(!test.isEmpty()){
-//            System.out.println(test.poll());
-//        }
 
 
-//        System.out.println(digraph.getAllNodeWeight());
 
-
-//        InputLoader.print(solutionTree, true);
     }
 
     private static int countLeaves(Digraph digraph) {
