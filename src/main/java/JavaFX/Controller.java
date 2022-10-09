8a5f3ef00e4f3e7058b22a5020e10c19d5a3dec7
Alan Zhang <azha644@aucklanduni.ac.nz>
2022-10-09 22:29:42 +1300

    feat: setup tilesfx and memory tile

--- src/main/java/JavaFX/Controller.java
+++ src/main/java/JavaFX/Controller.java
@@ -1,6 +1,8 @@
 package JavaFX;
 
 import algorithm.AStar;
+import eu.hansolo.tilesfx.Tile;
+import eu.hansolo.tilesfx.TileBuilder;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -28,9 +30,10 @@ public class Controller implements javafx.fxml.Initializable {
     @FXML
     private Label currentBestTime;
     @FXML
-    private VBox memTile;
+    private VBox memBox;
     private Timeline poller;
 
+    private Tile memoryTile;
 //    private Tile memoryTile;
     private int TILE_WIDTH = 200;
     private int TILE_HEIGHT = 200;
@@ -44,11 +47,26 @@ public class Controller implements javafx.fxml.Initializable {
     }
 
     private void setUpMemoryTile() {
+        memoryTile = TileBuilder.create()
+                .skinType(Tile.SkinType.GAUGE)
+                .prefSize(TILE_WIDTH, TILE_HEIGHT)
+                .maxValue(Runtime.getRuntime().maxMemory() / (1024 * 1024))
+                .threshold(Runtime.getRuntime().maxMemory() * 0.8 / (1024 * 1024))
+                .title("Memory")
+                .unit("MB")
+                .build();
+        memBox.getChildren().addAll(this.memoryTile);
 
     }
 
     private void update() {
         poller = new Timeline(new KeyFrame(Duration.millis(100), event -> {
+
+            // Updating memory tile
+            double memoryUsage = (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory())/(1000000d);
+            memoryTile.setValue(memoryUsage);
+
+            // Updating best time
             currentBestTime.setText((Main.getCurrentBestTime()));
         }));
         poller.setCycleCount(Animation.INDEFINITE);
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,8 +3,10 @@ module project{
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.graphics;
+    requires javafx.web;
     requires javafx.base;
     requires gs.core;
+    requires eu.hansolo.tilesfx;
 
 
     opens main;
